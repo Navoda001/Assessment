833eb9ba83ac4ad651b55521c8e8c33fd98a755b
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-09-22 23:50:15 +0530

    modify: heroSection.tsx

--- src/components/heroSection/heroSection.tsx
+++ src/components/heroSection/heroSection.tsx
@@ -42,7 +42,7 @@ const HeroSection: React.FC = () => {
 
     return (
         <div className='flex flex-col'>
-            <div className="relative w-full h-screen overflow-hidden">
+            <div className="relative w-full h-screen overflow-visible">
                 {/* Slides Container */}
                 <div
                     className="flex transition-transform duration-500 ease-in-out h-full"
@@ -106,42 +106,6 @@ const HeroSection: React.FC = () => {
                 >
                     <ChevronRight className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12" />
                 </button>
-            </div>
-           
-           
-           {/* News Banner*/}
-            <div className="bg-gradient-to-r from-primary3 to-primary2 z-20 py-5 px-32">
-                <div className="block sm:hidden text-center px-4 py-6">
-                    <div className="w-20 h-20 bg-white rounded-full flex items-center justify-center mx-auto mb-4">
-                        <span className="text-orange-500 font-bold text-xl"><img className='w-10 h-10' src="./pic1.png" alt="pic" /></span>
-                    </div>
-                    
-                    <div className="text-white mb-4">
-                        <h3 className="font-semibold text-lg mb-1">NEW INTAKE</h3>
-                        <p className="text-sm opacity-90">Next Intake Starts on January 05, 2026</p>
-                    </div>
-                    <button className="border-2 border-white text-white px-6 py-2 rounded-full hover:bg-white hover:text-blue-600 transition-all duration-300 text-sm font-medium">
-                        CONTACT US
-                    </button>
-                </div>
-                
-                <div className="hidden sm:flex items-center justify-between px-6 md:px-8  sm:py-4">
-
-                    <div className="flex items-center space-x-8">
-                        <div className="w-20 h-20 bg-white rounded-full flex items-center justify-center ">
-                            <span className="text-orange-500 font-bold text-base"><img className='w-10 h-10' src="./pic1.png" alt="pic" /></span>
-                        </div>
-                        
-                        <div className="text-white">
-                            <h3 className="font-semibold text-base md:text-xl mb-2">LATUS NEWS</h3>
-                            <p className="text-sm opacity-90 text-md">Next Intake Starts on January 05, 2026</p>
-                        </div>
-                    </div>
-                    
-                    <button className="border border-white text-white px-4 py-2 rounded-full transition-alltransform hover:scale-105 duration-300 text-sm font-medium">
-                        CONTACT US
-                    </button>
-                </div>
             </div>
         </div>
     );
